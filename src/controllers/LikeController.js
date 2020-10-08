bbd638e4b3ddc81e90b5b4beb7a88710fa95d035
Palloma Gualter <pallomagualter@gmail.com>
2020-10-08 09:16:04 -0300

    likes

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -2,6 +2,12 @@ const Post = require('../models/Post');
 
 module.exports = {
     async store(req, res) {
-       return res.json({ ok: true });
+        const post = await Post.findById(req.params.id);
+
+        post.likes += 1;
+
+        await post.save();
+        
+       return res.json(post);
     }
 };
